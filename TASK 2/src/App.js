4dd37a292d6c6215aa4c97be9d2cbb48679a85e8
sharathguvvala <guvvalasharath@gmail.com>
2021-07-19 21:28:39 +0530

    modified background

--- TASK 2/src/App.js
+++ TASK 2/src/App.js
@@ -1,23 +1,17 @@
 import Users from "./Components/card";
 import './App.css';
-
 import React, { Component } from 'react'
 	
 class App extends Component {
   constructor(props){
 	super(props)
-		
-	// Set initial state
 	this.state = {users_data :[],
                 loading : true                
   }
-  
-	// Binding this keyword
 	this.updateState = this.updateState.bind(this)
   }
     
   updateState(){
-      console.log("VT");
       document.getElementById("main").style.display='flex';
       const link="https://reqres.in/api/users?page=1";
       fetch(link)
@@ -27,7 +21,6 @@ class App extends Component {
         this.setState({users_data :users.data,
                         loading: false
         })
-        console.log(users.data);
       })
       .catch((error) => {
         console.error(error)
@@ -36,9 +29,9 @@ class App extends Component {
     
   render(){
     return (<>
-      <nav className="navbar navbar-expand-lg navbar-light">
+      <nav className="navbar navbar-expand-lg navbar-light rounded m-3">
           <div className="container-fluid">
-            <h3>LetsGrowMore</h3>
+            <h3>Zippy</h3>
             <button onClick={this.updateState}>Get Users</button>
           </div>
         </nav>
--- TASK 2/src/Components/card.js
+++ TASK 2/src/Components/card.js
@@ -1,21 +1,19 @@
 import React from 'react'
-
 const Users = ({loading,users}) => {
-    return loading ? (   //Checkif if is loading
+    return loading ? (
           <div id="main">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif" alt="Loaading.." className="loader"/>
+            <img src="https://flevix.com/wp-content/uploads/2019/07/Ring-Loading-feature.gif" alt="Loading..." className="loader"/>
           </div>
         ) : 
         (
-          <div className='row' id="main">
-    
+          <div className='row' id="main">  
           {users.map(user =>
-                      <div className="col-sm-6 col-md-4">
+                      <div className="col-sm-12 col-md-6 col-lg-4">
                         <div className="profile">
                           <img src={user.avatar} alt={user.avatar} className="avatar"></img>
+                          <p>ID: {user.id}</p>
                           <h1 className="name">{user.first_name} {user.last_name}</h1>
                           <p className="email">{user.email}</p>
-                          <p>User ID: {user.id}</p>
                         </div>
                       </div>
             )
