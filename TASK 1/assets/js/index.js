5d7c015cdf6d261760c4d92a2e70d6190d365063
sharathguvvala <guvvalasharath@gmail.com>
2021-07-19 17:04:27 +0530

    added task 1

---
+++ TASK 1/assets/js/index.js
@@ -0,0 +1,10 @@
+var thumbnails = document.getElementById("thumbnails");
+var imgs = thumbnails.getElementsByTagName("img");
+var main = document.getElementById("main");
+var counter = 0;
+for (let i = 0; i < imgs.length; i++) {
+  let img = imgs[i];
+  img.addEventListener("click", function () {
+    main.src = this.src;
+  });
+}
