78a234c1aef8832d0d71433ccc5586a938849f04
sharathguvvala <guvvalasharath@gmail.com>
2021-07-19 23:26:32 +0530

    modified

--- TASK 2/src/Components/card.js
+++ TASK 2/src/Components/card.js
@@ -4,7 +4,7 @@ const Users = ({loading,users}) => {
           <div id="main">
             <img src="https://flevix.com/wp-content/uploads/2019/07/Ring-Loading-feature.gif" alt="Loading..." className="loader"/>
           </div>
-        ) : 
+        ) :
         (
           <div className='row' id="main">  
           {users.map(user =>
